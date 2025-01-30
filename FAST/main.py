76ab00a28cc42ff9ce89ac40b61cf4767b301f1e
AlonsoChavez363 <122044170@upq.edu.mx>
2025-01-30 15:49:17 -0600

    practica 2 dia 29 de enero

--- FAST/main.py
+++ FAST/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title = 'Mi primera API S192' ,
+    description = 'Alonso Chávez' ,
+    version = '1.0.1'
+)
 
 #Endpoint home
 @app.get('/')
 def home():
     return {'hello':'world FastAPI'}
+
+#endpoint promedio
+@app.get('/promedio')
+def promedio():
+    return 8.2
