92bf802750f60345063bf191894a834331898017
AlonsoChavez363 <122044170@upq.edu.mx>
2025-03-11 19:07:45 -0600

    Update middlewares.py

--- FAST/middlewares.py
+++ FAST/middlewares.py
@@ -8,7 +8,7 @@ class BearerJWT(HTTPBearer):
         data = validateToken(auth.credentials)
 
         if not isinstance(data, dict): #verificar si es un diccionario valido
-            raise HTTPException(status_code=401, detail="Toekn invalido")
+            raise HTTPException(status_code=401, detail="Token invalido")
         
-        if data.get('email') != 'alonso@example.com':
+        if data.get('correo') != 'alonso@example.com':
             raise HTTPException(status_code=403, detail="Credenciales no validas")
